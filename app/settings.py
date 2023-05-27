52c050b09ab36c8cf4a48bdfd39e6d61f04b0902
Ruslan Zulufov <gothiq2302@gmail.com>
2023-05-27 19:01:06 +0600

    Update db default settings

--- app/settings.py
+++ app/settings.py
@@ -195,7 +195,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
-DATABASES['default'] = dj_database_url.config(
-    conn_max_age=500,
-    conn_health_checks=True
-)
+# DATABASES['default'] = dj_database_url.config(
+#     conn_max_age=500,
+#     conn_health_checks=True
+# )
