8e3bb18142a5d85f765a02bbf8f59d1d19192154
Ruslan Zulufov <gothiq2302@gmail.com>
2023-05-27 19:45:57 +0600

    Update CSRF verification

--- app/settings.py
+++ app/settings.py
@@ -31,7 +31,10 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['web-production-51b6.up.railway.app', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-51b6.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-51b6.up.railway.app/',
+    'https://web-production-51b6.up.railway.app/admin/login/?next=/admin/'
+]
 
 # Application definition
 
