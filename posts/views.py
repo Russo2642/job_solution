e7ad2eb35dd959292031684b134bf34c48170f8a
Ruslan Zulufov <gothiq2302@gmail.com>
2023-06-07 15:33:53 +0600

    Add search field

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import permissions, status, viewsets
+from rest_framework import permissions, status, viewsets, filters
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -22,6 +22,8 @@ class RatingViewSet(viewsets.ReadOnlyModelViewSet):
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
 
     def get_serializer_class(self):
         if self.action in ('create', 'update', 'partial_update', 'destroy'):
