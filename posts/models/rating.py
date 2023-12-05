dd69a333e8f6774cd663e124d0c97038ec177f0f
Ruslan Zulufov <gothiq2302@gmail.com>
2023-12-05 12:25:32 +0600

    Model rating

--- posts/models/rating.py
+++ posts/models/rating.py
@@ -3,33 +3,38 @@ from django.db import models
 
 
 class Rating(models.Model):
-    article = models.ForeignKey(
+    article = models.OneToOneField(
         to='posts.Post',
         related_name='article_rating',
         on_delete=models.CASCADE,
-        default=0,
     )
     salary = models.SmallIntegerField(
         blank=False,
         null=False,
-        validators=[MinValueValidator(1), MaxValueValidator(5)])
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        default=0),
     team = models.SmallIntegerField(
         blank=False,
         null=False,
-        validators=[MinValueValidator(1), MaxValueValidator(5)])
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        default=0),
     education = models.SmallIntegerField(
         blank=False,
         null=False,
-        validators=[MinValueValidator(1), MaxValueValidator(5)])
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        default=0),
     management = models.SmallIntegerField(
         blank=False,
         null=False,
-        validators=[MinValueValidator(1), MaxValueValidator(5)])
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        default=0),
     social_package = models.SmallIntegerField(
         blank=False,
         null=False,
-        validators=[MinValueValidator(1), MaxValueValidator(5)])
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        default=0),
     office_comfort = models.SmallIntegerField(
         blank=False,
         null=False,
-        validators=[MinValueValidator(1), MaxValueValidator(5)])
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        default=0),
